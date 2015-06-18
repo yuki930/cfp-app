d0aa273ed8aaac5cefef6c0b50324bb16640abfd
Akira Matsuda <ronnie@dio.jp>
2015-06-18 18:40:56 +0900

    Show GitHub, Twitter accounts on speaker information

--- app/decorators/speaker_decorator.rb
+++ app/decorators/speaker_decorator.rb
@@ -14,6 +14,35 @@ class SpeakerDecorator < ApplicationDecorator
     "#{object.name} (#{object.email})"
   end
 
+  def link_to_github
+    if user.provider == 'github'
+      uname = github_uid_to_uname user.uid
+      h.link_to "@#{uname}", "https://github.com/#{uname}"
+    else
+      'none'
+    end
+  end
+
+  private def github_uid_to_uname(uid)
+    api_uri = URI.parse "https://api.github.com/user/#{uid}"
+    JSON.parse(Net::HTTP.get(api_uri))['login']
+  end
+
+  def link_to_twitter
+    if user.provider == 'twitter'
+      uname = twitter_uid_to_uname user.uid
+      h.link_to "@#{uname}", "https://twitter.com/#{uname}"
+    else
+      'none'
+    end
+  end
+
+  private def twitter_uid_to_uname(uid)
+    twitter_uri = "https://twitter.com/intent/user?user_id=#{uid}"
+    html = Net::HTTP.get URI.parse(twitter_uri)
+    html.scan(/<span class="nickname">@(.*)<\/span>/).first.first
+  end
+
   def bio
     object.bio.present? ? object.bio : object.user.try(:bio)
   end
