9510148c3f7c71187efe6c037b44e98a026cbbba
Akira Matsuda <ronnie@dio.jp>
2017-05-11 11:40:04 +0900

    Emails will be sent from rubykaigi.org domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'rubykaigi.org',
     :enable_starttls_auto => true
   }
 
