052c4041d9dc5c951fcabfbadba77983b9e36458
Sorah Fukumori <sorah@cookpad.com>
2019-02-09 14:58:30 +0900

    Let proposal_mailer_template generates markdown

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,7 +4,7 @@ class ApplicationMailer < ActionMailer::Base
   def mail_markdown(options)
     mail options do |format|
       markdown_string = render_to_string(action_name, formats: :md)
-      format.html { render text: Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(markdown_string)}
+      format.html { render text: Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true, autolink: true).render(markdown_string)}
       format.text { render text: markdown_string}
     end
   end
