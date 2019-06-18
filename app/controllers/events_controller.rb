172916ca110fff076817daf274fbd8f2c6d988f1
Yuya Fujiwara <asonas@cookpad.com>
2019-06-18 21:32:04 +0900

    git add app

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,13 @@
 class EventsController < ApplicationController
-  before_filter :require_event, only: [:show]
+  skip_before_action :current_event, only: [:index]
+  before_action :require_event, only: [:show]
 
   def index
     render locals: {
-      events: Event.recent.decorate
+      events: Event.not_draft.closes_up.decorate
     }
   end
 
   def show
-    @title = "#{@event.name} CFP"
   end
 end
--- app/decorators/speaker_decorator.rb
+++ app/decorators/speaker_decorator.rb
@@ -1,6 +1,7 @@
 class SpeakerDecorator < ApplicationDecorator
   delegate_all
   decorates_association :proposals
+  decorates_association :program_sessions
 
   def gravatar
     image_url =
@@ -13,43 +14,7 @@ class SpeakerDecorator < ApplicationDecorator
     "#{object.name} (#{object.email})"
   end
 
-  def link_to_github
-    if (github = person && person.services.find_by(provider: 'github'))
-      gh_login = Rails.cache.fetch "gh_#{github.uid}" do
-        JSON.parse(Net::HTTP.get(URI("https://api.github.com/user/#{github.uid}")))['login']
-      end
-      h.link_to "@#{gh_login}", "https://github.com/#{gh_login}", target: '_blank'
-    else
-      'none'
-    end
-  end
-
-  def link_to_twitter
-    if (twitter = person && person.services.find_by(provider: 'twitter'))
-      tw_screen_name = Rails.cache.fetch "tw_#{twitter.uid}" do
-        Twitter::REST::Client.new(consumer_key: ENV['TWITTER_KEY'], consumer_secret: ENV['TWITTER_SECRET']).user(twitter.uid.to_i).screen_name
-      end
-      h.link_to "@#{tw_screen_name}", "https://twitter.com/#{tw_screen_name}", target: '_blank'
-    else
-      'none'
-    end
-  end
-
   def bio
-    speaker.bio.present? ? speaker.bio : speaker.person.try(:bio) || 'N/A'
-  end
-
-  def delete_button
-    h.button_to h.organizer_event_speaker_path,
-                method: :delete,
-                data: {
-                  confirm:
-                    'This will delete this speaker. Are you sure you want to do this? ' +
-                      'It can not be undone.'
-                },
-                class: 'btn btn-danger navbar-btn',
-                id: 'delete' do
-      bang('Delete Speaker')
-    end
+    object.bio.present? ? object.bio : object.user.try(:bio)
   end
 end
