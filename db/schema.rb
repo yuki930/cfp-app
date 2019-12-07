8a1b8d30fe68b1ddb6fb082100e28fe15fe97072
Akira Matsuda <ronnie@dio.jp>
2019-12-07 11:30:43 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -153,6 +153,7 @@ ActiveRecord::Schema.define(version: 2023_02_03_031006) do
     t.datetime "confirmed_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float "average_rating"
     t.index ["event_id"], name: "index_proposals_on_event_id"
     t.index ["session_format_id"], name: "index_proposals_on_session_format_id"
     t.index ["track_id"], name: "index_proposals_on_track_id"
