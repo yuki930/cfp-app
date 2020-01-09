f3aee867f9f95f5ba39836d4395d2355bd045046
Akira Matsuda <ronnie@dio.jp>
2020-01-10 01:59:37 +0900

    rails g migration add_github_uid_to_users github_uid

--- db/schema.rb
+++ db/schema.rb
@@ -323,6 +323,7 @@ ActiveRecord::Schema.define(version: 2022_06_06_213730) do
     t.datetime "remember_created_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "github_uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token"
     t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
