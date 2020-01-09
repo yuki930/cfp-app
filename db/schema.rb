dffb25eb7bb4e1d6236312bab3ebf1d4a0b56c9e
Akira Matsuda <ronnie@dio.jp>
2020-01-10 01:59:37 +0900

    rails g migration add_github_uid_to_users github_uid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_094054) do
+ActiveRecord::Schema.define(version: 2020_01_09_165645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -245,6 +245,7 @@ ActiveRecord::Schema.define(version: 2019_12_06_094054) do
     t.datetime "remember_created_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "github_uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token"
     t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
