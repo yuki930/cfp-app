985ae36e1fa0b6a09e73abf37d51f8684b212c50
Akira Matsuda <ronnie@dio.jp>
2020-01-11 19:59:34 +0900

    Refer to github and twitter uid imported from the v1 DB

--- app/decorators/speaker_decorator.rb
+++ app/decorators/speaker_decorator.rb
@@ -15,8 +15,8 @@ class SpeakerDecorator < ApplicationDecorator
   end
 
   def link_to_github
-    if user.provider == 'github'
-      uname = github_uid_to_uname user.uid
+    if user.github_uid
+      uname = github_uid_to_uname user.github_uid
       h.link_to "@#{uname}", "https://github.com/#{uname}", target: '_blank'
     else
       'none'
@@ -31,8 +31,8 @@ class SpeakerDecorator < ApplicationDecorator
   end
 
   def link_to_twitter
-    if user.provider == 'twitter'
-      uname = twitter_uid_to_uname user.uid
+    if user.twitter_uid
+      uname = twitter_uid_to_uname user.twitter_uid
       h.link_to "@#{uname}", "https://twitter.com/#{uname}", target: '_blank'
     else
       'none'
