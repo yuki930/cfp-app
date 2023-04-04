7224a5c963e7b76952d6d65dbe10cf38ba470216
Akira Matsuda <ronnie@dio.jp>
2023-04-04 12:15:54 +0900

    &.

--- app/decorators/speaker_decorator.rb
+++ app/decorators/speaker_decorator.rb
@@ -15,15 +15,15 @@ class SpeakerDecorator < ApplicationDecorator
   end
 
   def github_account
-    user.github_account.presence || (user.github_uid.presence && github_uid_to_uname(user.github_uid))
+    user&.github_account.presence || (user&.github_uid.presence && github_uid_to_uname(user.github_uid))
   end
 
   def twitter_account
-    user.twitter_account.presence || (user.twitter_uid.presence && twitter_uid_to_uname(user.twitter_uid))
+    user&.twitter_account.presence || (user&.twitter_uid.presence && twitter_uid_to_uname(user.twitter_uid))
   end
 
   def social_account
-    twitter_account || github_account || user.name.downcase.tr(' ', '_')
+    twitter_account || github_account || (user&.name || speaker_name).downcase.tr(' ', '_')
   end
 
   def link_to_github
